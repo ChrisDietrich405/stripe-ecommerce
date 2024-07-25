fa2aa1e586f4aa14df2cafca1c7d4b4fd0bde183
Chris <chrisdietrich3666@gmail.com>
2024-07-25 18:12:32 -0400

    added auth api

--- src/app/api/register-customer/route.js
+++ src/app/api/register-customer/route.js
@@ -1,5 +1,6 @@
 import { NextResponse } from "next/server";
 import CustomersModel from "@/app/models/customers";
+import bcrypt from "bcryptjs";
 
 export async function POST(req) {
   const { name, streetAddress, city, email, password } = await req.json();
@@ -14,36 +15,55 @@ export async function POST(req) {
       }
     );
   }
+
+  const emailFormat = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+  if (!email.match(emailFormat)) {
+    return NextResponse.json(
+      {
+        status: 400,
+        message: "Incorrect email format",
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+
+  const existingEmail = await CustomersModel.findOne({ email });
+  if (existingEmail) {
+    return NextResponse.json(
+      { status: 409, message: "Duplicate email" },
+      {
+        status: 409,
+      }
+    );
+  }
+
   try {
-    const newUser = new CustomersModel({
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    console.log(hashedPassword);
+
+    const newCustomer = new CustomersModel({
       name,
       streetAddress,
       city,
       email,
-      password,
+      password: hashedPassword,
     });
 
-    await newUser.save();
+    await newCustomer.save();
 
-    return NextResponse.json({ status: 201, message: "User created" });
+    return NextResponse.json({ status: 201, message: "Customer created" });
   } catch (error) {
-    console.log(error);
+    return NextResponse.json(
+      { status: 500, message: error.message },
+      {
+        status: 500,
+      }
+    );
   }
-  //   try {
-  //     const salt = await bcrypt.genSalt(10);
-  //     const hashedPassword = await bcrypt.hash(password, salt);
-
-  //     const newUser = new UsersModel({
-  //       name,
-  //       streetAddress,
-  //       city,
-  //       email,
-  //       password: hashedPassword,
-  //     });
-
-  //     await newUser.save();
-
-  //     return NextResponse.json({ status: 201, message: "User created" });
 }
 
 // export const POST = async (req: NextRequest, res: NextResponse) => {
