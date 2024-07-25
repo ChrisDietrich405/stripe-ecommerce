4d1018fa6ed9f088ecdffc0ecc621104b89fdaa6
Chris <chrisdietrich3666@gmail.com>
2024-07-25 10:44:11 -0400

    added db

---
+++ src/app/api/customers/route.js
@@ -0,0 +1,117 @@
+import { NextResponse } from "next/server";
+import CustomersModel from "@/app/models/customers";
+
+export async function POST(req) {
+  const { name, streetAddress, city, email, password } = await req.json();
+  if (!name || !streetAddress || !city || !email || !password) {
+    return NextResponse.json(
+      {
+        status: 400,
+        message: "Please add all necessary information",
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+  try {
+    const newUser = new CustomersModel({
+      name,
+      streetAddress,
+      city,
+      email,
+      password,
+    });
+
+    await newUser.save();
+
+    return NextResponse.json({ status: 201, message: "User created" });
+  } catch (error) {
+    console.log(error);
+  }
+  //   try {
+  //     const salt = await bcrypt.genSalt(10);
+  //     const hashedPassword = await bcrypt.hash(password, salt);
+
+  //     const newUser = new UsersModel({
+  //       name,
+  //       streetAddress,
+  //       city,
+  //       email,
+  //       password: hashedPassword,
+  //     });
+
+  //     await newUser.save();
+
+  //     return NextResponse.json({ status: 201, message: "User created" });
+}
+
+// export const POST = async (req: NextRequest, res: NextResponse) => {
+//   const { name, streetAddress, city, email, password } =
+//     await req.json();
+//   if (
+//     !name ||
+//     !streetAddress ||
+//     !city ||
+//     !email ||
+//     !password
+//   ) {
+//     return NextResponse.json(
+//       {
+//         status: 400,
+//         message: "Please add all necessary information",
+//       },
+//       {
+//         status: 400,
+//       }
+//     );
+//   }
+
+//   const emailFormat = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+//   if (!email.match(emailFormat)) {
+//     return NextResponse.json(
+//       {
+//         status: 400,
+//         message: "Incorrect email format",
+//       },
+//       {
+//         status: 400,
+//       }
+//     );
+//   }
+
+//   const existingEmail = await UsersModel.findOne({ email });
+//   if (existingEmail) {
+//     return NextResponse.json(
+//       { status: 409, message: "Duplicate email" },
+//       {
+//         status: 409,
+//       }
+//     );
+//   }
+
+//   try {
+//     const salt = await bcrypt.genSalt(10);
+//     const hashedPassword = await bcrypt.hash(password, salt);
+
+//     const newUser = new UsersModel({
+//       name,
+//       streetAddress,
+//       city,
+//       email,
+//       password: hashedPassword,
+//     });
+
+//     await newUser.save();
+
+//     return NextResponse.json({ status: 201, message: "User created" });
+//   } catch (error: any) {
+//     return NextResponse.json(
+//       { status: 500, message: error.message },
+//       {
+//         status: 500,
+//       }
+//     );
+//   }
+// };
